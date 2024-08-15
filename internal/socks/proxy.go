24c8f7c2a31efa08f0378186a265ed21a35a3692
Gar|k <garik.djan@gmail.com>
2024-08-15 19:52:12 +0300

    change tunnel

--- internal/socks/proxy.go
+++ internal/socks/proxy.go
@@ -2,20 +2,30 @@ package socks
 
 import (
 	"context"
+	"errors"
 	"io"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func StartTunnel(ctx context.Context, src, dst io.ReadWriteCloser) error {
-	go (func() {
-		<-ctx.Done()
+	eg, egCtx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		<-egCtx.Done()
 		_ = src.Close()
 		_ = dst.Close()
-	})()
 
-	go (func() {
-		_, _ = io.Copy(src, dst)
-	})()
+		return nil
+	})
 
-	_, err := io.Copy(dst, src)
-	return err
+	eg.Go(func() error {
+		_, err := io.Copy(src, dst)
+		return err
+	})
+
+	_, errCopy := io.Copy(dst, src)
+	errWait := eg.Wait()
+
+	return errors.Join(errCopy, errWait)
 }
