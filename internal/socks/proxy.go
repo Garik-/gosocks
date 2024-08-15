8a1f301cd1b497a5899a62bfb4b6bf7d0e565c29
Gar|k <garik.djan@gmail.com>
2024-08-15 21:21:09 +0300

    fix lint

--- internal/socks/proxy.go
+++ internal/socks/proxy.go
@@ -12,6 +12,7 @@ func StartTunnel(ctx context.Context, src, dst io.ReadWriteCloser) error {
 
 	eg.Go(func() error {
 		<-egCtx.Done()
+
 		_ = src.Close()
 		_ = dst.Close()
 
@@ -20,11 +21,13 @@ func StartTunnel(ctx context.Context, src, dst io.ReadWriteCloser) error {
 
 	eg.Go(func() error {
 		_, err := io.Copy(src, dst)
+
 		return err
 	})
 
 	eg.Go(func() error {
 		_, err := io.Copy(dst, src)
+
 		return err
 	})
 
